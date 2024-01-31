f4d25def1538f67e7da6d3116dcfd614f7d30b38
Eric DeSoto <153691975+eRaWk23@users.noreply.github.com>
2024-01-30 20:54:28 -0800

    Add files via upload

---
+++ RSA.cpp
@@ -0,0 +1,40 @@
+#include"RSA.h"
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+bool isPrime(unsigned long long n)
+{
+    if(n % 2 == 1)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+unsigned long long getPrime(unsigned long long min, unsigned long long max)
+{
+    unsigned long long primeNumber = 0;
+    while(!isPrime)
+    {
+        primeNumber = rand() % max + min;
+        isPrime(primeNumber);
+    }
+    return primeNumber;
+}
+unsigned long long gcd(unsigned long long x, unsigned long long y)
+{
+    unsigned long long gcdenom;
+    return gcdenom;
+}
+unsigned long long lcm(unsigned long long x, unsigned long long y);
+unsigned long long modInverse(unsigned long long e, unsigned long long lam);
+unsigned long long modExp(unsigned long long base,unsigned long long exp,unsigned long long n)
+{
+   unsigned long long ans = 1;
+   for(unsigned long long i = 0; i < exp; i++)
+      ans = (ans * base) % n;
+   return ans;
+}
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include"RSA.h"
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    unsigned long long p;
+    unsigned long long q;
+    unsigned long long n;
+    unsigned long long lamdba;
+    unsigned long long d;
+    unsigned long long e;
+    unsigned long long m;
+    unsigned long long c;
+
+    cout << "Please enter a seed number: " << endl;
+    return 0;
+}
